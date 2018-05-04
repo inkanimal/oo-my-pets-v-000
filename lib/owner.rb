25666a53fef525e0924f5a477ed3b768799a53a3
geoffrey woodcock <theatomproject@gmail.com>
2018-05-04 17:14:57 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,21 @@
 class Owner
   # code goes here
+  @@all = []
+  @@owners_count = 0
+  
+  attr_accessor :owner, :cat, :dog, :fish, :name
+  
+  def initialize(owner)
+    @owner = owner
+    @pets = {}
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.reset_all
+    @@all.clear
+  end
+  
 end
